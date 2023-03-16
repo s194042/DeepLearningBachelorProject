81cc083bd33260030f14b619c87f68b448b0bbbb
raniiboe <61318713+raniiboe@users.noreply.github.com>
2023-03-16 13:21:27 +0100

    generateLossImages now has cupy

--- Code/Image_functions/generateLossImages.py
+++ Code/Image_functions/generateLossImages.py
@@ -7,91 +7,91 @@ import torchsummary
 
 ## loss of 0 is perfect, loss with 1 is really bad
 
-def get_images_with_loss_of_0(img):
+def get_images_with_loss_of_0(img, cupy=False):
     yield img
-    yield augmentations.Guasian_noice(img, 0.02)
-    yield augmentations.Uniform_noice(img, 2)
-    yield augmentations.Exp_noice(img, 0.01)
-    yield augmentations.Poiss_noice(img, 0.03)
-    yield augmentations.uniform_add(img, 5)
-    yield augmentations.uniform_add(img, -4)
-    yield augmentations.uniform_decimal_multiplication(img, 0.95)
-    yield augmentations.uniform_decimal_multiplication(img, 1.05)
-    yield augmentations.Unsharp_masking_5x5(img, 1)
+    yield augmentations.Guasian_noice(img, 0.02, cupy)
+    yield augmentations.Uniform_noice(img, 2, cupy)
+    yield augmentations.Exp_noice(img, 0.01, cupy)
+    yield augmentations.Poiss_noice(img, 0.03, cupy)
+    yield augmentations.uniform_add(img, 5, cupy)
+    yield augmentations.uniform_add(img, -4, cupy)
+    yield augmentations.uniform_decimal_multiplication(img, 0.95, cupy)
+    yield augmentations.uniform_decimal_multiplication(img, 1.05, cupy)
+    yield augmentations.Unsharp_masking_5x5(img, 1, cupy)
 
-def get_images_with_loss_of_0_2(img):
-    yield augmentations.Guasian_noice(img, 0.05)
-    yield augmentations.Uniform_noice(img, 6)
-    yield augmentations.Exp_noice(img, 0.035)
-    yield augmentations.Poiss_noice(img, 0.15)
-    yield augmentations.uniform_add(img, 15)
-    yield augmentations.uniform_add(img, -15)
-    yield augmentations.uniform_decimal_multiplication(img, 0.88)
-    yield augmentations.uniform_decimal_multiplication(img, 1.12)
-    yield augmentations.sharpen(img, 1)
-    yield augmentations.box_blur_3x3(img, 1)
-    yield augmentations.Gaussioan_blur_3x3(img, 1)
+def get_images_with_loss_of_0_2(img, cupy=False):
+    yield augmentations.Guasian_noice(img, 0.05, cupy)
+    yield augmentations.Uniform_noice(img, 6, cupy)
+    yield augmentations.Exp_noice(img, 0.035, cupy)
+    yield augmentations.Poiss_noice(img, 0.15, cupy)
+    yield augmentations.uniform_add(img, 15, cupy)
+    yield augmentations.uniform_add(img, -15, cupy)
+    yield augmentations.uniform_decimal_multiplication(img, 0.88, cupy)
+    yield augmentations.uniform_decimal_multiplication(img, 1.12, cupy)
+    yield augmentations.sharpen(img, 1, cupy)
+    yield augmentations.box_blur_3x3(img, 1, cupy)
+    yield augmentations.Gaussioan_blur_3x3(img, 1, cupy)
 
-def get_images_with_loss_of_0_4(img):
-    yield augmentations.Guasian_noice(img, 0.15)
-    yield augmentations.Uniform_noice(img, 20)
-    yield augmentations.Exp_noice(img, 0.1)
-    yield augmentations.Poiss_noice(img, 0.4)
-    yield augmentations.uniform_add(img, 50)
-    yield augmentations.uniform_add(img, -50)
-    yield augmentations.uniform_decimal_multiplication(img, 0.6)
-    yield augmentations.uniform_decimal_multiplication(img, 1.4)
-    yield augmentations.salt(img, 0.01)
-    yield augmentations.peper(img, 0.01)
-    yield augmentations.box_blur_5x5(img, 1)
-    yield augmentations.Gaussioan_blur_5x5(img, 1)
-
-
-def get_images_with_loss_of_0_6(img):
-    yield augmentations.Guasian_noice(img, 0.3)
-    yield augmentations.Uniform_noice(img, 45)
-    yield augmentations.Exp_noice(img, 0.3)
-    yield augmentations.Poiss_noice(img, 0.7)
-    yield augmentations.uniform_add(img, 90)
-    yield augmentations.uniform_add(img, -90)
-    yield augmentations.uniform_decimal_multiplication(img, 0.4)
-    yield augmentations.uniform_decimal_multiplication(img, 1.6)
-    yield augmentations.sharpen(img, 2)
-    yield augmentations.Unsharp_masking_5x5(img, 4)
-    yield augmentations.salt(img, 0.05)
-    yield augmentations.peper(img, 0.05)
+def get_images_with_loss_of_0_4(img, cupy=False):
+    yield augmentations.Guasian_noice(img, 0.15, cupy)
+    yield augmentations.Uniform_noice(img, 20, cupy)
+    yield augmentations.Exp_noice(img, 0.1, cupy)
+    yield augmentations.Poiss_noice(img, 0.4, cupy)
+    yield augmentations.uniform_add(img, 50, cupy)
+    yield augmentations.uniform_add(img, -50, cupy)
+    yield augmentations.uniform_decimal_multiplication(img, 0.6, cupy)
+    yield augmentations.uniform_decimal_multiplication(img, 1.4, cupy)
+    yield augmentations.salt(img, 0.01, cupy)
+    yield augmentations.peper(img, 0.01, cupy)
+    yield augmentations.box_blur_5x5(img, 1, cupy)
+    yield augmentations.Gaussioan_blur_5x5(img, 1, cupy)
 
 
-def get_images_with_loss_of_0_8(img):
-    yield augmentations.Guasian_noice(img, 0.6)
-    yield augmentations.Uniform_noice(img, 90)
-    yield augmentations.Exp_noice(img, 0.5)
-    yield augmentations.Poiss_noice(img, 1)
-    yield augmentations.uniform_add(img, 120)
-    yield augmentations.uniform_add(img, -120)
-    yield augmentations.uniform_decimal_multiplication(img, 0.2)
-    yield augmentations.uniform_decimal_multiplication(img, 1.8)
-    yield augmentations.sharpen(img, 2)
-    yield augmentations.Unsharp_masking_5x5(img, 5)
-    yield augmentations.box_blur_5x5(img, 20)
-    yield augmentations.Gaussioan_blur_5x5(img, 20)
-    yield augmentations.salt(img, 0.25)
-    yield augmentations.peper(img, 0.3)
+def get_images_with_loss_of_0_6(img, cupy=False):
+    yield augmentations.Guasian_noice(img, 0.3, cupy)
+    yield augmentations.Uniform_noice(img, 45, cupy)
+    yield augmentations.Exp_noice(img, 0.3, cupy)
+    yield augmentations.Poiss_noice(img, 0.7, cupy)
+    yield augmentations.uniform_add(img, 90, cupy)
+    yield augmentations.uniform_add(img, -90, cupy)
+    yield augmentations.uniform_decimal_multiplication(img, 0.4, cupy)
+    yield augmentations.uniform_decimal_multiplication(img, 1.6, cupy)
+    yield augmentations.sharpen(img, 2, cupy)
+    yield augmentations.Unsharp_masking_5x5(img, 4, cupy)
+    yield augmentations.salt(img, 0.05, cupy)
+    yield augmentations.peper(img, 0.05, cupy)
 
 
-def get_images_with_loss_of_1(img):
-    yield augmentations.Guasian_noice(img, 1.2)
-    yield augmentations.Uniform_noice(img, 160)
-    yield augmentations.Exp_noice(img, 1)
-    yield augmentations.Poiss_noice(img, 1.3)
-    yield augmentations.uniform_add(img, 140)
-    yield augmentations.uniform_add(img, -140)
-    yield augmentations.uniform_decimal_multiplication(img, 0.1)
-    yield augmentations.uniform_decimal_multiplication(img, 2.3)
-    yield augmentations.sharpen(img, 3)
-    yield augmentations.Unsharp_masking_5x5(img, 7)
-    yield augmentations.salt(img, 0.5)
-    yield augmentations.peper(img, 0.6)
+def get_images_with_loss_of_0_8(img, cupy=False):
+    yield augmentations.Guasian_noice(img, 0.6, cupy)
+    yield augmentations.Uniform_noice(img, 90, cupy)
+    yield augmentations.Exp_noice(img, 0.5, cupy)
+    yield augmentations.Poiss_noice(img, 1, cupy)
+    yield augmentations.uniform_add(img, 120, cupy)
+    yield augmentations.uniform_add(img, -120, cupy)
+    yield augmentations.uniform_decimal_multiplication(img, 0.2, cupy)
+    yield augmentations.uniform_decimal_multiplication(img, 1.8, cupy)
+    yield augmentations.sharpen(img, 2, cupy)
+    yield augmentations.Unsharp_masking_5x5(img, 5, cupy)
+    yield augmentations.box_blur_5x5(img, 20, cupy)
+    yield augmentations.Gaussioan_blur_5x5(img, 20, cupy)
+    yield augmentations.salt(img, 0.25, cupy)
+    yield augmentations.peper(img, 0.3, cupy)
+
+
+def get_images_with_loss_of_1(img, cupy=False):
+    yield augmentations.Guasian_noice(img, 1.2, cupy)
+    yield augmentations.Uniform_noice(img, 160, cupy)
+    yield augmentations.Exp_noice(img, 1, cupy)
+    yield augmentations.Poiss_noice(img, 1.3, cupy)
+    yield augmentations.uniform_add(img, 140, cupy)
+    yield augmentations.uniform_add(img, -140, cupy)
+    yield augmentations.uniform_decimal_multiplication(img, 0.1, cupy)
+    yield augmentations.uniform_decimal_multiplication(img, 2.3, cupy)
+    yield augmentations.sharpen(img, 3, cupy)
+    yield augmentations.Unsharp_masking_5x5(img, 7, cupy)
+    yield augmentations.salt(img, 0.5, cupy)
+    yield augmentations.peper(img, 0.6, cupy)
 
 
 class MakeIter(object):
@@ -105,22 +105,22 @@ class MakeIter(object):
     def __len__(self):
         return 101984400
 
-def get_image_pairs_transforms_with_loss(path='C:/Users/Rani/Desktop/ai_training_immages'):
+def get_image_pairs_transforms_with_loss(path='C:/Users/Rani/Desktop/ai_training_immages', cupy=False):
     imgs = images.get_all_imgs_with_everything(path)
     for img in imgs:
 
 
         
-        for aug_0 in get_images_with_loss_of_0(img):
+        for aug_0 in get_images_with_loss_of_0(img, cupy):
             yield torch.tensor(np.concatenate(((img - 128)/255, img-aug_0), axis=2), dtype=torch.float).permute(2,0,1).to("cuda"), torch.tensor(0.0).to("cuda")
-        for aug_0 in get_images_with_loss_of_1(img):
+        for aug_0 in get_images_with_loss_of_1(img, cupy):
             yield torch.tensor(np.concatenate(((img - 128)/255, img-aug_0), axis=2), dtype=torch.float).permute(2,0,1).to("cuda"), torch.tensor(0.999).to("cuda")
-        for aug_0 in get_images_with_loss_of_0_2(img):
+        for aug_0 in get_images_with_loss_of_0_2(img, cupy):
             yield torch.tensor(np.concatenate(((img - 128)/255, img-aug_0), axis=2), dtype=torch.float).permute(2,0,1).to("cuda"), torch.tensor(0.2000).to("cuda")
-        for aug_0 in get_images_with_loss_of_0_6(img):
+        for aug_0 in get_images_with_loss_of_0_6(img, cupy):
             yield torch.tensor(np.concatenate(((img - 128)/255, img-aug_0), axis=2), dtype=torch.float).permute(2,0,1).to("cuda"), torch.tensor(0.6000).to("cuda")
-        for aug_0 in get_images_with_loss_of_0_4(img):
+        for aug_0 in get_images_with_loss_of_0_4(img, cupy):
             yield torch.tensor(np.concatenate(((img - 128)/255, img-aug_0), axis=2), dtype=torch.float).permute(2,0,1).to("cuda"), torch.tensor(0.4000).to("cuda")
-        for aug_0 in get_images_with_loss_of_0_8(img):
+        for aug_0 in get_images_with_loss_of_0_8(img, cupy):
             yield torch.tensor(np.concatenate(((img - 128)/255, img-aug_0), axis=2), dtype=torch.float).permute(2,0,1).to("cuda"), torch.tensor(0.8000).to("cuda")
         

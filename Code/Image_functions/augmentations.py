dc3dea2f7a6f669b77460f0b6a32c4281a3a560c
raniiboe <61318713+raniiboe@users.noreply.github.com>
2023-03-16 11:41:56 +0100

    Cupy filters

--- Code/Image_functions/augmentations.py
+++ Code/Image_functions/augmentations.py
@@ -1,103 +1,115 @@
 #from . import images
 import numpy as np
+import cupy as cp
 import copy
 
-def applyFilet(image,filter):
-    image_c = copy.deepcopy(image).astype('float64')
-    image_r = image_c[:,:,0]
-    image_g = image_c[:,:,1]
-    image_b = image_c[:,:,2]
-
-    
-    image_c[:,:,0] = np.fft.irfft2(np.fft.rfft2(image_r) * np.fft.rfft2(filter, image_r.shape))
-    image_c[:,:,1] = np.fft.irfft2(np.fft.rfft2(image_g) * np.fft.rfft2(filter, image_g.shape))
-    image_c[:,:,2] = np.fft.irfft2(np.fft.rfft2(image_b) * np.fft.rfft2(filter, image_b.shape))
-
-    image_c[image_c<0] = 0
-    image_c[image_c>255] = 255
-    image_c = np.rint(image_c)
-    return image_c.astype("uint8")  
-
-def sharpen(image, times = 1):
-    filter = np.array([ [0,-1,0],
-                        [-1,5,-1],
-                        [0,-1,0]])
-    image_c = copy.deepcopy(image)
-    for _ in range(times):
-        image_c = applyFilet(image_c, filter)
+def applyFilet(image,filter, cupy=False):
+    if cupy:
+        image_r = image[:,:,0]
+        image_g = image[:,:,1]
+        image_b = image[:,:,2]
+        image[:,:,0] = cp.fft.irfft2(cp.multiply(cp.fft.rfft2(image_r), cp.fft.rfft2(filter, cp.shape(image_r))))
+        image[:,:,1] = cp.fft.irfft2(cp.multiply(cp.fft.rfft2(image_g), cp.fft.rfft2(filter, cp.shape(image_g))))
+        image[:,:,2] = cp.fft.irfft2(cp.multiply(cp.fft.rfft2(image_b), cp.fft.rfft2(filter, cp.shape(image_b))))
+        cp.clip(image, a_min=0, a_max=255, out=image)
+        image_c = cp.rint(image)
+        
+    else:
+        image_c = copy.deepcopy(image).astype('float64')
+        image_r = image_c[:,:,0]
+        image_g = image_c[:,:,1]
+        image_b = image_c[:,:,2]
+        image_c[:,:,0] = np.fft.irfft2(np.fft.rfft2(image_r) * np.fft.rfft2(filter, image_r.shape))
+        image_c[:,:,1] = np.fft.irfft2(np.fft.rfft2(image_g) * np.fft.rfft2(filter, image_g.shape))
+        image_c[:,:,2] = np.fft.irfft2(np.fft.rfft2(image_b) * np.fft.rfft2(filter, image_b.shape))
+        np.clip(image_c, a_min=0, a_max=255, out=image_c)
+        image_c = np.rint(image_c)
     return image_c
 
+def applyFilter(image, filter, times=1, cupy=False):
+    if cupy:
+        filter = cp.asfarray(filter)
+        image_c = cp.asfarray(image)
+        for _ in range(times):
+            image_c = applyFilet(image_c, filter, cupy)
+        image_c = cp.asnumpy(image_c)
 
-def box_blur_3x3(image, times = 1):
+    else:
+        filter = np.array(filter)
+        image_c = copy.deepcopy(image)
+        for _ in range(times):
+            image_c = applyFilet(image_c, filter, cupy)
+    return image_c.astype(np.uint8)
+
+def sharpen(image, times = 1, cupy=False):
+    filter = [  [0,-1,0],
+                [-1,5,-1],
+                [0,-1,0]]
+    
+    return applyFilter(image, filter, times, cupy)
+
+
+def box_blur_3x3(image, times = 1, cupy=False):
     div = 9
-    filter = np.array([ [1/div,1/div,1/div],
-                        [1/div,1/div,1/div],
-                        [1/div,1/div,1/div]])
-    image_c = copy.deepcopy(image)
-    for _ in range(times):
-        image_c = applyFilet(image_c, filter)
-    return image_c
+    filter = [  [1/div,1/div,1/div],
+                [1/div,1/div,1/div],
+                [1/div,1/div,1/div]]
+    
+    return applyFilter(image, filter, times, cupy)
     
 
-def box_blur_5x5(image, times = 1):
+def box_blur_5x5(image, times = 1, cupy=False):
     div = 25
-    filter = np.array([ [1/div,1/div,1/div,1/div,1/div],
-                        [1/div,1/div,1/div,1/div,1/div],
-                        [1/div,1/div,1/div,1/div,1/div],
-                        [1/div,1/div,1/div,1/div,1/div],
-                        [1/div,1/div,1/div,1/div,1/div]])
-    image_c = copy.deepcopy(image)
-    for _ in range(times):
-        image_c = applyFilet(image_c, filter)
-    return image_c
+    filter = [ [1/div,1/div,1/div,1/div,1/div],
+                [1/div,1/div,1/div,1/div,1/div],
+                [1/div,1/div,1/div,1/div,1/div],
+                [1/div,1/div,1/div,1/div,1/div],
+                [1/div,1/div,1/div,1/div,1/div]]
+    
+    return applyFilter(image, filter, times, cupy)
 
 
-def Gaussioan_blur_3x3(image, times = 1):
+def Gaussioan_blur_3x3(image, times = 1, cupy=False):
     div = 16
     filter = np.array([ [1/div,2/div,1/div],
                         [2/div,4/div,2/div],
                         [1/div,2/div,1/div]])
-    image_c = copy.deepcopy(image)
-    for _ in range(times):
-        image_c = applyFilet(image_c, filter)
-    return image_c
+    
+    return applyFilter(image, filter, times, cupy)
 
 
-def Gaussioan_blur_5x5(image, times = 1):
+def Gaussioan_blur_5x5(image, times = 1, cupy=False):
     div = 256
-    filter = np.array([ [1/div,4/div,6/div,4/div,1/div],
-                        [4/div,16/div,24/div,16/div,4/div],
-                        [6/div,24/div,36/div,24/div,6/div],
-                        [4/div,16/div,24/div,16/div,4/div],
-                        [1/div,4/div,6/div,4/div,1/div]])
-    image_c = copy.deepcopy(image)
-    for _ in range(times):
-        image_c = applyFilet(image_c, filter)
-    return image_c
+    filter = [  [1/div,4/div,6/div,4/div,1/div],
+                [4/div,16/div,24/div,16/div,4/div],
+                [6/div,24/div,36/div,24/div,6/div],
+                [4/div,16/div,24/div,16/div,4/div],
+                [1/div,4/div,6/div,4/div,1/div]]
+    
+    return applyFilter(image, filter, times, cupy)
 
 
-def Unsharp_masking_5x5(image, times = 1):
+def Unsharp_masking_5x5(image, times = 1, cupy=False):
     div = -256
-    filter = np.array([ [1/div,4/div,6/div,4/div,1/div],
-                        [4/div,16/div,24/div,16/div,4/div],
-                        [6/div,24/div,-476/div,24/div,6/div],
-                        [4/div,16/div,24/div,16/div,4/div],
-                        [1/div,4/div,6/div,4/div,1/div]])
-    image_c = copy.deepcopy(image)
-    for _ in range(times):
-        image_c = applyFilet(image_c, filter)
-    return image_c
+    filter = [  [1/div,4/div,6/div,4/div,1/div],
+                [4/div,16/div,24/div,16/div,4/div],
+                [6/div,24/div,-476/div,24/div,6/div],
+                [4/div,16/div,24/div,16/div,4/div],
+                [1/div,4/div,6/div,4/div,1/div]]
+    
+    return applyFilter(image, filter, times, cupy)
 
+def salt(image, rate, cupy=False):
+    if cupy:
 
-def salt(image, rate):
-    image_c = copy.deepcopy(image)
-    row,col,ch = image_c.shape
-    rnd = np.random.rand(row,col,ch)
+        image_c = cp.array(copy.deepcopy(image))
+        row,col,ch = cp.shape(image_c)
+        rnd = np.random.rand(row,col,ch)
     image_c[rnd<rate] = 255
     return image_c
 
 
-def peper(image, rate):
+def peper(image, rate, cupy=False):
     image_c = copy.deepcopy(image)
     row,col,ch = image_c.shape
     rnd = np.random.rand(row,col,ch)
@@ -105,7 +117,7 @@ def peper(image, rate):
     return image_c
 
 
-def Guasian_noice(image, rate):
+def Guasian_noice(image, rate, cupy=False):
     image_c = copy.deepcopy(image).astype('float64')
     row,col,ch = image_c.shape
     gauss = np.random.normal(0, rate*100, image_c.shape)
@@ -117,7 +129,7 @@ def Guasian_noice(image, rate):
     return image_c.astype("uint8")
 
 
-def Poiss_noice(image, rate):
+def Poiss_noice(image, rate, cupy=False):
     image_c = copy.deepcopy(image).astype('float64')
     row,col,ch = image_c.shape
     poisson = np.random.poisson(rate*100, image_c.shape)
@@ -129,7 +141,7 @@ def Poiss_noice(image, rate):
     return image_c.astype("uint8")   
 
 
-def Exp_noice(image, rate):
+def Exp_noice(image, rate, cupy=False):
     image_c = copy.deepcopy(image).astype('float64')
     row,col,ch = image_c.shape
     exponential = np.random.exponential(rate*100, image_c.shape)
@@ -141,7 +153,7 @@ def Exp_noice(image, rate):
     return image_c.astype("uint8")   
 
 
-def Uniform_noice(image, max):
+def Uniform_noice(image, max, cupy=False):
     image_c = copy.deepcopy(image).astype('float64')
     row,col,ch = image_c.shape
     uniform = np.random.uniform(-max, max, image_c.shape)
@@ -153,14 +165,14 @@ def Uniform_noice(image, max):
     return image_c.astype("uint8")
 
 
-def uniform_add(image, amount):
+def uniform_add(image, amount, cupy=False):
     image_c = copy.deepcopy(image).astype('int32')
     image_c = image_c + amount
     image_c[image_c<0] = 0
     image_c[image_c>255] = 255
     return image_c.astype("uint8")  
 
-def uniform_decimal_multiplication(image, amount):
+def uniform_decimal_multiplication(image, amount, cupy=False):
     image_c = copy.deepcopy(image).astype('float64')
     image_c = image_c * amount
     image_c[image_c<0] = 0

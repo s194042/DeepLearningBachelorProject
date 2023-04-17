60aa6cb153c642b07f6c32def5adf8abbbbae9dc
s194042 <hhhermansen@gmail.com>
2023-04-17 16:07:16 +0200

    Updates for HPC

--- Code/Image_functions/HPC_entropy.py
+++ Code/Image_functions/HPC_entropy.py
@@ -67,14 +67,14 @@ for epoch in range(start_epoch, epochs):
     print(epoch)
     if startup:
         training = generateCompressionImages.MakeIter(path = path, folder = folder, start_index=start_index if epoch == start_epoch else 0, startup = True)
-        training_loader = torch.utils.data.DataLoader(training, batch_size=4, num_workers=2)
+        training_loader = torch.utils.data.DataLoader(training, batch_size=4, num_workers=1)
         min_lr *= batch_size**0.5
         max_lr *= batch_size**0.5
         step_size = (max_lr-min_lr)/steps
         optimizer.param_groups[-1]['lr'] = max_lr
     else:
         training = generateCompressionImages.MakeIter(path = path, folder = folder, start_index=start_index if epoch == start_epoch else 0, epoch=epoch, startup = False)
-        training_loader = torch.utils.data.DataLoader(training, batch_size=batch_size, num_workers=2)
+        training_loader = torch.utils.data.DataLoader(training, batch_size=batch_size, num_workers=1)
 
 
     for index, data in enumerate(training_loader):

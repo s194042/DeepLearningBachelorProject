de6f6cdf1e90300a33f7740ce6e9f76a7ade50e1
s194042 <hhhermansen@gmail.com>
2023-04-19 14:59:09 +0200

    updated git ignore and jobscript

--- Code/Image_functions/jobscript.sh
+++ Code/Image_functions/jobscript.sh
@@ -3,7 +3,7 @@
 ### –- specify queue --
 #BSUB -q gpua100
 ### -- set the job Name --
-#BSUB -J CE_MSE
+#BSUB -J LossFirstRun
 ### -- ask for number of cores (default: 1) --
 #BSUB -n 1
 ### -- Select the resources: 1 gpu in exclusive process mode --
@@ -11,7 +11,7 @@
 ### -- set walltime limit: hh:mm --  maximum 24 hours for GPU-queues right now
 #BSUB -W 23:30
 # request 12GB of system-memory
-#BSUB -R "rusage[mem=12GB]"
+#BSUB -R "rusage[mem=32GB]"
 ### -- set the email address --
 # please uncomment the following line and put in your e-mail address,
 # if you want to receive e-mail notifications on a non-default address
@@ -24,7 +24,7 @@
 ### -- -o and -e mean append, -oo and -eo mean overwrite --
 #BSUB -o JobOutputFiles/gpu_%J.out
 #BSUB -e JobOutputFiles/gpu_%J.err
-#BSUB -R "select[gpu40gb]"
+#BSUB -R "select[gpu80gb]"
 # -- end of LSF options --
 
 nvidia-smi
@@ -34,4 +34,4 @@ module load cuda/11.6
 cd /work3/s194042/
 source .env/bin/activate
 cd DeepLearningBachelorProject/Code
-python Image_functions/HPC_entropy.py CE_MSE MSE
+python Image_functions/HPCLossv2.py LossFirstRun
